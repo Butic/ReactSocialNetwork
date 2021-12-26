bbbefcacfa73240e99ef00a31acff1ae03e47a9f
Andrey <butic.92@mail.ru>
2021-12-27 01:44:55 +0300

    added json-server create change function to class and make axios request throught the construnctor method

--- src/components/Users/UsersItem.jsx
+++ src/components/Users/UsersItem.jsx
@@ -4,77 +4,65 @@ import Avatar from "../Profile/Avatar";
 import classes from './UsersItem.module.css';
 import avatar from '../UI/img/1283.png_860.png';
 
-const UsersItem=(props)=>{
-    if(props.users.length === 0){
-        // axios.get('https://social-network.samuraijs.com/api/1.0/users').then(responce=>{
-        //     props.usersList(responce.data.items);
-        // })
-        props.usersList([
-            {id:1, avatar:null, followed:false, name:'Andrew', status:'some status here', location:{country:'Country', sity:'Sity'}},
-            {id:2, avatar:null, followed:true, name:'Alena', status:'some status here', location:{country:'Country', sity:'Sity'}},
-            {id:3, avatar:null, followed:true, name:'Dimas', status:'some status here', location:{country:'Country', sity:'Sity'}},
-            {id:4, avatar:null, followed:false, name:'Artem', status:'some status here', location:{country:'Country', sity:'Sity'}},
-            {id:5, avatar:null, followed:true, name:'Elena', status:'some status here', location:{country:'Country', sity:'Sity'}},
-            {id:6, avatar:null, followed:false, name:'Alexey', status:'some status here', location:{country:'Country', sity:'Sity'}},
-            {id:7, avatar:null, followed:false, name:'Yurok', status:'some status here', location:{country:'Country', sity:'Sity'}},
-            {id:8, avatar:null, followed:true, name:'Andrew', status:'some status here', location:{country:'Country', sity:'Sity'}},
-            {id:9, avatar:null, followed:false, name:'Alena', status:'some status here', location:{country:'Country', sity:'Sity'}},
-            {id:10, avatar:null, followed:true, name:'Dimas', status:'some status here', location:{country:'Country', sity:'Sity'}},
-            {id:11, avatar:null, followed:true, name:'Artem', status:'some status here', location:{country:'Country', sity:'Sity'}},
-            {id:12, avatar:null, followed:false, name:'Elena', status:'some status here', location:{country:'Country', sity:'Sity'}},
-            {id:13, avatar:null, followed:false, name:'Alexey', status:'some status here', location:{country:'Country', sity:'Sity'}},
-            {id:14, avatar:null, followed:false, name:'Yurok', status:'some status here', location:{country:'Country', sity:'Sity'}}
-        ]);
-    }
-    const follow=(e)=>{
-        props.follow(e.target.id);
+class UsersItem extends React.Component{
+    constructor(props){
+        super(props);
+        axios.get('http://localhost:8000/users').then(responce=>{
+                this.props.usersList(responce.data);
+            })
     }
 
-    return(
-        props.users.map(el=>{
-            if(el.followed){
-                return(
-                    <div className={classes.User__container}>
-                        <div className={classes.User__follow}>
-                            <Avatar />
-                            <button id={el.id} onClick={follow} className={classes.User__follow__button}>Follow</button>
-                        </div>
-                        <div className={classes.User__about}>
-                        <div className={classes.User__description}>
-                            <h3 className={classes.User__description__name}>{el.name}</h3>
-                            <p className={classes.User__description__status}>{el.status}</p>
-                        </div>
-                        <div className={classes.User__location}>
-                            {/* <h4 className={classes.User__location__country}>{el.location.country},</h4> */}
-                            {/* <p className={classes.User__location__sity}>{el.location.sity}</p> */}
-                        </div>
-                        </div>
-                    </div>
-                )
-            }
-            else{
-                return(
-                    <div className={classes.User__container}>
-                        <div className={classes.User__follow}>
-                            <Avatar/>
-                            <button id={el.id} onClick={follow} className={classes.User__unfollow__button}>Unfollow</button>
-                        </div>
-                        <div className={classes.User__about+" "+classes.User__subscribed}>
-                        <div className={classes.User__description}>
-                            <h3 className={classes.User__description__name}>{el.name}</h3>
-                            <p className={classes.User__friend}>(You are subscribed)</p>
-                            <p className={classes.User__description__status}>{el.status}</p>
-                        </div>
-                        <div className={classes.User__location}>
-                            {/* <h4 className={classes.User__location__country}>{el.location.country},</h4> */}
-                            {/* <p className={classes.User__location__sity}>{el.location.sity}</p> */}
-                        </div>
-                        </div>
-                    </div>
-                )
-            }
-        })
-    )
+    follow=(e)=>{
+        this.props.follow(e.target.id);
+    }
+    
+    render(){
+        return(
+                this.props.users.map(el=>{
+                    if(el.followed){
+                        return(
+                            <div className={classes.User__container}>
+                                <div className={classes.User__follow}>
+                                    <Avatar />
+                                    <button id={el.id} onClick={this.follow} className={classes.User__follow__button}>Follow</button>
+                                </div>
+                                <div className={classes.User__about}>
+                                <div className={classes.User__description}>
+                                    <h3 className={classes.User__description__name}>{el.name}</h3>
+                                    <p className={classes.User__description__status}>{el.status}</p>
+                                </div>
+                                <div className={classes.User__location}>
+                                    <h4 className={classes.User__location__country}>{el.location.country},</h4>
+                                    <p className={classes.User__location__sity}>{el.location.sity}</p>
+                                </div>
+                                </div>
+                            </div>
+                        )
+                    }
+                    else{
+                        return(
+                            <div className={classes.User__container}>
+                                <div className={classes.User__follow}>
+                                    <Avatar/>
+                                    <button id={el.id} onClick={this.follow} className={classes.User__unfollow__button}>Unfollow</button>
+                                </div>
+                                <div className={classes.User__about+" "+classes.User__subscribed}>
+                                <div className={classes.User__description}>
+                                    <h3 className={classes.User__description__name}>{el.name}</h3>
+                                    <p className={classes.User__friend}>(You are subscribed)</p>
+                                    <p className={classes.User__description__status}>{el.status}</p>
+                                </div>
+                                <div className={classes.User__location}>
+                                    <h4 className={classes.User__location__country}>{el.location.country},</h4>
+                                    <p className={classes.User__location__sity}>{el.location.sity}</p>
+                                </div>
+                                </div>
+                            </div>
+                        )
+                    }
+                })
+        )
+    }
 }
 
-export default UsersItem
+export default UsersItem;
